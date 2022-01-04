5ba1629299a2e6a92480eec9a735744117e51c59
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2022-01-05 01:20:47 +0700

    [=] 🌪 separate to component

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -6,40 +6,7 @@
       <div
         class="bg-hsl-b5 lg:rounded-md relative overflow-hidden mx-auto mt-4 box"
       >
-        <transition
-          enter-active-class="transition duration-200 ease-out"
-          enter-class="opacity-0"
-          enter-to-class="opacity-100"
-          leave-active-class="transition ease-in duration-200"
-          leave-class="opacity-100"
-          leave-to-class="opacity-0"
-        >
-          <div
-            v-show="fetching"
-            class="absolute top-0 left-0 z-50 flex items-center justify-center w-full h-full transition-all bg-gray-800 opacity-0 pointer-events-none backdrop-filter bg-opacity-20 backdrop-blur-sm"
-          >
-            <svg
-              class="w-12 h-12 text-white animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              />
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              />
-            </svg>
-          </div>
-        </transition>
+        <FetchOverlay :fetching="fetching" />
         <div class="px-8 py-4">
           <div class="relative overflow-x-auto">
             <template v-if="result.length !== 0">
