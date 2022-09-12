874bf6eb77847680326194c90b293de96386fc49
arily <arily@arily.moe>
2022-09-12 19:50:41 +0900

    fixup create result

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -5,5 +5,5 @@ export const createActionResult = (data: Record<string, any> = {}) => () => ({
 export const sleep = ms => new Promise((resolve) => {
   setTimeout(resolve, ms)
 })
-export const userLogin = () => sleep(500).then(createActionResult)
-export const userRegister = () => sleep(500).then(createActionResult)
+export const userLogin = result => sleep(500).then(createActionResult(result))
+export const userRegister = result => sleep(500).then(createActionResult(result))
