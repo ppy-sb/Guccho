e41a79353b4c617fa0d1c7a3f480439b6cb0e227
arily <arily@arily.moe>
2022-09-24 02:52:43 +0900

    done

---
+++ src/components/T/modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="zoom-modal-content">
+    <div class="card bg-ebony-clay-800">
+      <div class="card-body">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+.zoom-modal-content {
+  @apply h-screen m-auto flex items-center justify-center
+}
+</style>
