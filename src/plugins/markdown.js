f26ffdf245fb737eb05e09fc04691539c5138c5c
Ari Arimaniac <a@ri.mk>
2022-09-28 18:10:39 +0900

    markdown

--- src/plugins/markdown.js
+++ src/plugins/markdown.js
@@ -1,5 +1,6 @@
 // plugins/markdown-it.ts
 import Markdown from 'vue3-markdown-it'
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Markdown)
+  // nuxtApp.vueApp.use(Markdown)
+  nuxtApp.vueApp.component('Markdown', Markdown)
 })
