66e74cb6b1d0156c70adf517e1952ffe56199dd4
Ari Arimaniac <a@ri.mk>
2022-10-03 12:12:21 +0900

    type only props??

--- src/composables/useEditorExtensions.ts
+++ src/composables/useEditorExtensions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 import Highlight from '@tiptap/extension-highlight'
 import Typography from '@tiptap/extension-typography'
 import StarterKit from '@tiptap/starter-kit'
--- src/composables/useSafariDetector.ts
+++ src/composables/useSafariDetector.ts
@@ -1,4 +1,4 @@
-export default function useSafari (def) {
+export default function useSafari (def: boolean) {
   if (!process.client) { return def }
   const isSafari = navigator.userAgent.includes('Safari') && navigator.userAgent.indexOf('Chrome') <= -1
   return isSafari
