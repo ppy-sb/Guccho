efec5c5db14479bfba121c95334fdbf820e15bbc
Ari Arimaniac <a@ri.mk>
2022-10-12 15:05:14 +0900

    dom

--- src/pages/me/friends.vue
+++ src/pages/me/friends.vue
@@ -13,41 +13,42 @@ const user = scoped.demoUser
         grid
       </t-tab>
     </t-tabs>
-    <div v-if="layout === 'list'" class="w-full user-list">
+    <div v-if="layout === 'list'" class="max-w-4xl mx-auto user-list">
       <div
         v-for="i in 4"
         :key="i"
-        class="user-list-item w-full p-2 flex justify-between items-center"
+        class="user-list-item w-full p-2"
       >
         <div
-          class="flex gap-4 justify-center md:justify-start items-center face"
+          class="flex gap-2 justify-center md:justify-start items-center face"
         >
           <div class="relative mask mask-squircle z-10 hoverable w-100">
             <img
               src="/images/1.png"
-              class="pointer-events-none"
-              style="min-width: 75px; width: 75px"
+              class="pointer-events-none w-14 md:w-[4em]"
             >
           </div>
-          <div>
-            <h1 class="text-4xl text-left">
+          <div class="grow">
+            <h1 class="text-2xl md:text-4xl text-left">
               {{ user.name }}
             </h1>
-            <a
-              class="text-2xl text-left underline decoration-sky-500 text-kimberly-600 dark:text-kimberly-300 hover:text-kimberly-500"
-              href="#"
-            >
-              @{{ user.safeName }}
-            </a>
+            <div class="flex justify-between w-full items-top">
+              <a
+                class="text-lg md:text-2xl text-left underline decoration-sky-500 text-kimberly-600 dark:text-kimberly-300 hover:text-kimberly-500"
+                href="#"
+              >
+                @{{ user.safeName }}
+              </a>
+              <div class="flex gap-2 actions">
+                <t-button variant="info" size="xs" class="md:btn-sm">
+                  chat
+                </t-button>
+                <t-button variant="warning" size="xs" class="md:btn-sm">
+                  remove friend
+                </t-button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="flex gap-2 actions">
-          <t-button variant="info" size="sm">
-            chat
-          </t-button>
-          <t-button variant="warning" size="sm">
-            remove friend
-          </t-button>
         </div>
       </div>
     </div>
