5befd2df0680ada9572b32db9ea4fcc8db11b2f4
Ari Arimaniac <a@ri.mk>
2022-10-13 10:26:34 +0900

    trpc get users

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -1,6 +1,24 @@
 // ~/server/trpc/index.ts
 // import type { inferAsyncReturnType } from '@trpc/server'
 import * as trpc from '@trpc/server'
-import { z } from 'zod' //  yup/superstruct/zod/myzod/custom
+import { z } from 'zod'
+import { sampleUserWithSecrets, scoped } from '@/prototyping/objects/user' //  yup/superstruct/zod/myzod/custom
 
 export const router = trpc.router()
+
+  .query('getUser', {
+    input: z.object({
+      handle: z.union([z.string(), z.number()])
+    }),
+    resolve () {
+      return sampleUserWithSecrets
+    }
+  })
+  .query('getUsers', {
+    input: z.object({
+      handle: z.union([z.string(), z.number()])
+    }),
+    resolve () {
+      return scoped
+    }
+  })
