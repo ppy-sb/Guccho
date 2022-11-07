3d3f599abc00ab97688a87630eddde23dcb47a24
arily <ariy@arily.moe>
2022-11-07 12:00:49 +0900

    floating-vue menu

---
+++ src/plugins/floating-vue-themes.js
@@ -0,0 +1,9 @@
+import { options } from 'floating-vue'
+
+export default defineNuxtPlugin(() => {
+  options.themes['guweb-dropdown'] = {
+    $extend: 'dropdown',
+    $resetCss: true,
+    autoHide: true
+  }
+})
