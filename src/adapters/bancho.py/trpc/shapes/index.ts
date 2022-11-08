a35ba515d3b50084fe8fca9142557a634d3f1a01
arily <ariy@arily.moe>
2022-11-08 20:32:52 +0900

    leaderboard

--- src/adapters/bancho.py/trpc/shapes/index.ts
+++ src/adapters/bancho.py/trpc/shapes/index.ts
@@ -2,3 +2,7 @@ import z from 'zod'
 
 export const zodHandle = z.union([z.string(), z.number()])
 export const zodRelationType = z.union([z.literal('friend'), z.literal('block')])
+
+export const zodMode = z.union([z.literal('osu'), z.literal('taiko'), z.literal('fruits'), z.literal('mania')])
+export const zodRuleset = z.union([z.literal('standard'), z.literal('relax'), z.literal('autopilot')])
+export const zodRankingSystem = z.union([z.literal('ppv2'), z.literal('rankedScore'), z.literal('totalScore')])
