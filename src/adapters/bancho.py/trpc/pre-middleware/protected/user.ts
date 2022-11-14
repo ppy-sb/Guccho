4b277d9c997c8c08a6734d6595cada1f2cd071ca
arily <ariy@arily.moe>
2022-11-14 17:30:38 +0900

    inline

--- src/adapters/bancho.py/trpc/pre-middleware/protected/user.ts
+++ src/adapters/bancho.py/trpc/pre-middleware/protected/user.ts
@@ -3,34 +3,32 @@ import { createRouterWithSession } from '../session'
 
 import { getBaseUser } from '$/bancho.py/backend-clients/user'
 
-export function createProtectedRouter () {
-  return createRouterWithSession()
-    .middleware(async ({ ctx, next }) => {
-      const session = await ctx.session.getBinding()
-      if (!session) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'cannot retrieve session'
-        })
-      }
-      if (!session.userId) {
-        throw new TRPCError({
-          code: 'UNAUTHORIZED',
-          message: 'you need to login first'
-        })
-      }
-      const user = await getBaseUser(session.userId)
-      if (!user) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'user not found'
-        })
-      }
-      return next({
-        ctx: {
-          ...ctx,
-          user
-        }
+export const createProtectedRouter = () => createRouterWithSession()
+  .middleware(async ({ ctx, next }) => {
+    const session = await ctx.session.getBinding()
+    if (!session) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'cannot retrieve session'
       })
+    }
+    if (!session.userId) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'you need to login first'
+      })
+    }
+    const user = await getBaseUser(session.userId)
+    if (!user) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'user not found'
+      })
+    }
+    return next({
+      ctx: {
+        ...ctx,
+        user
+      }
     })
-}
+  })
