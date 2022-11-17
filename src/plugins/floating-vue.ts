152b82513946fe17f564504bad7ac22c4c225789
arily <ariy@arily.moe>
2022-11-17 10:32:41 +0900

    hotfix floating-vue

---
+++ src/plugins/floating-vue.ts
@@ -0,0 +1,6 @@
+import { defineNuxtPlugin } from '#app'
+import FloatingVue from 'floating-vue'
+import 'floating-vue/dist/style.css'
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(FloatingVue)
+})
