da14cb48269bb23590c46b4408ed036e33a40aed
arily <ariy@arily.moe>
2022-11-23 19:07:13 +0900

    dump

--- src/types/internal-utils.ts
+++ src/types/internal-utils.ts
@@ -5,13 +5,13 @@ export type Enumerate<
   ? Acc[number]
   : Enumerate<N, [...Acc, Acc['length']]>
 
-type THEN<T, F> = {
+type THEN<T = true, F = false> = {
   true: T
   else: F
 }
-type CATCH<_C, _R> = {
-  catch: _C
-  then: _R
+type CATCH<C = unknown, T = unknown> = {
+  catch: C
+  then?: T
 }
 export type CASE<T, F> = [T, F]
 export type MATCHER<MF extends (i: any) => any> = [
@@ -19,12 +19,14 @@ export type MATCHER<MF extends (i: any) => any> = [
   ReturnType<MF>
 ]
 export type DEFAULT<F> = [any, F]
-export type Extends<CmpLhs, CmpRhs> = () => CmpLhs extends CmpRhs
-  ? true
-  : false
+export type EXTENDS<CmpLhs, CmpRhs> = () => CmpLhs extends CmpRhs ? true : false
+export type INCLUDES<CmpLhs, CmpRhs> = EXTENDS<CmpRhs, CmpLhs>
+// yes it's union operator bc we are checking against true so true | false is false!
+export type EQUALS<CmpLhs, CmpRhs> = () => ReturnType<EXTENDS<CmpLhs, CmpRhs>> | ReturnType<INCLUDES<CmpLhs, CmpRhs>>
+
 export type IF<
-  _Cmp extends Extends<any, any>,
-  R extends THEN<unknown, unknown>
+  _Cmp extends EXTENDS<any, any> | INCLUDES<any, any> | EQUALS<any, any>,
+  R extends THEN<unknown, unknown> = THEN
 > = ReturnType<_Cmp> extends true ? R['true'] : R['else']
 export type SWITCH<Input, Cases extends CASE<any, any> | MATCHER<any>> = {
   [CASE in Cases as Input extends CASE[0]
@@ -35,37 +37,28 @@ export type SWITCH<Input, Cases extends CASE<any, any> | MATCHER<any>> = {
     ? 'includes'
     : 'isNot']: CASE[1]
 }
-export type MATCHES<T extends { extends?: any }> = T['extends']
-export type INCLUDES<T extends { includes?: any }> = T['includes']
-export type EQUALS<T extends { equals?: any }> = T['equals']
-export type NOT<T extends { isNot?: any }> = T['isNot']
-export type TRY<T, C extends CATCH<unknown, any>> = T extends C['catch']
+export type TRY<T, C extends CATCH<unknown, unknown>> = T extends C['catch']
   ? C['then']
   : T
 
-// type res1 = IF<Extends<Array<number>, Array<1>>, {
+// type res1 = IF<EXTENDS<Array<number>, Array<1>>, {
 //   true: 'array[1]',
-//   else: IF<Extends<number, number>, {
-//     true: 1,
-//     else: never
-//   }>
+//   else: IF<EQUALS<2, 1>>
 // }>
 
 // type SwitchRes<T> = SWITCH<T,
 //   | CASE<2, 'number[2]'>
-//   | CASE<number, 2>
+//   | CASE<number, 'any number'>
 //   | CASE<string, 'any string'>
 //   | CASE<'2', 'string[2]'>
 //   | DEFAULT<never>
 // >
 
-// type T2 = SwitchRes<string>
-
 // type res2 = TRY<
-//   switchRes['equals']
+//   SwitchRes<number>['equals']
 //   , {
-//     catch: 'string',
-//     then: string
+//     catch: 'any number',
+//     then: 'caught[any number]'
 //   }>
 
 // type result = IF<Extends<res1, res2>, {
@@ -74,3 +67,8 @@ export type TRY<T, C extends CATCH<unknown, any>> = T extends C['catch']
 // }>
 
 // TSScript lol
+// from here is dump
+// export type MATCHES<T extends { extends?: any }> = T['extends']
+// export type INCLUDES<T extends { includes?: any }> = T['includes']
+// export type EQUALS<T extends { equals?: any }> = T['equals']
+// export type NOT<T extends { isNot?: any }> = T['isNot']
