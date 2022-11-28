84ed3d600bc87ce56a4db1b0b76df0b80d100ad6
arily <ariy@arily.moe>
2022-11-28 17:39:06 +0900

    change layout

--- src/components/app/layout.vue
+++ src/components/app/layout.vue
@@ -22,22 +22,18 @@ onMounted(() => {
     :class="[
       safari ? 'safari' : 'not-safari',
       { 'has-bg': props.hasBg },
-    ]"
-    class="screen"
+    ]" class="screen"
   >
     <app-navbar :disabled="modalContainer?.stat === 'show'" />
     <app-experience class="z-50" />
-    <t-modal-container
-      ref="modalContainer"
-      :teleport-id="config.appModalTeleportTargetId"
-    >
+    <t-modal-container ref="modalContainer" :teleport-id="config.appModalTeleportTargetId">
       <!-- bg-kimberly-50 dark:bg-kimberly-800 -->
-      <div class="flex flex-col min-h-screen overflow-y-hidden">
+      <div class="flex flex-col overflow-y-hidden">
         <div class="flex flex-col min-h-screen overflow-auto">
           <div class="flex flex-col flex-grow">
             <slot />
           </div>
-          <slot name="footer" />
+          <slot v-if="hasBg" name="footer" />
         </div>
       </div>
     </t-modal-container>
@@ -45,9 +41,13 @@ onMounted(() => {
 </template>
 
 <style lang="postcss">
-.screen.has-bg {
-  @apply bg-gradient-to-b from-kimberly-50 to-kimberly-150/80 dark:from-kimberly-800 dark:to-kimberly-900
+.screen {
+  @apply bg-kimberly-100 dark:bg-kimberly-900;
+  &.has-bg {
+    @apply bg-gradient-to-b from-kimberly-50 to-kimberly-150/80 dark:from-kimberly-800 dark:to-kimberly-900
+  }
 }
+
 .not-safari {
   & .custom-container {
     @apply drop-shadow-xl
