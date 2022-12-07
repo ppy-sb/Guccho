1e2ed8ab2272e8588b5898c524af1d44bd37074f
arily <ariy@arily.moe>
2022-12-07 22:56:47 +0900

    complete failure you do not check extended class w/ abstract method ? WRYYYYYYYYYY

--- src/adapters/bancho.py/client/user.ts
+++ src/adapters/bancho.py/client/user.ts
@@ -1,14 +1,15 @@
 import bcrypt from 'bcryptjs'
 import { createClient } from 'redis'
 import { TRPCError } from '@trpc/server'
-import type { Prisma } from '@prisma/client'
+import type { Prisma, PrismaClient } from '@prisma/client'
 import type { IdType as Id } from '../config'
 import { BanchoPyMode, toBanchoPyMode } from '../enums'
 import { createRulesetData, toBaseUser, toFullUser } from '../transforms'
 import { toRankingSystemScores } from '../transforms/scores'
+import type { UserDataProvider } from '../../base/client/user'
 import { createUserQuery } from './db-queries'
 import { getRelationships } from './user-relations'
-import { prismaClient as db } from './index'
+import { prismaClient } from './index'
 import type { Mode, Range, RankingSystem, Ruleset } from '~/types/common'
 
 import type {
@@ -23,44 +24,6 @@ const redisClient
   && createClient({
     url: process.env.REDIS_URI,
   })
-interface OptType<
-  Includes extends Partial<Record<keyof UserOptional<Id>, boolean>> = Record<
-    never,
-    never
-  >,
-> {
-  handle: string | Id
-  includes?: Includes
-  keys?: Array<['id', 'name', 'safeName', 'email'][number]>
-}
-export async function userExists({ handle, keys }: OptType) {
-  return (
-    (await db.user.count(
-      createUserQuery(handle, keys || ['id', 'name', 'safeName', 'email']),
-    )) > 0
-  )
-}
-
-export async function getBaseUser<
-  Includes extends Partial<Record<keyof UserOptional<number>, boolean>>,
->(opt: OptType<Includes>) {
-  const { handle, includes, keys } = opt
-  const user = await db.user.findFirst(
-    createUserQuery(handle, keys || ['id', 'name', 'safeName', 'email']),
-  )
-  if (user == null)
-    return null
-
-  return toBaseUser({ user, includes })
-}
-
-export async function getBaseUsers<
-  Includes extends Partial<Record<keyof UserOptional<Id>, boolean>>,
->(opt: { handle: string | Id; includes?: Includes }) {
-  const { handle, includes } = opt
-  const users = await db.user.findMany(createUserQuery(handle))
-  return users.map(user => toBaseUser({ user, includes }))
-}
 
 async function getLiveRank(id: number, mode: number, country: string) {
   if (redisClient) {
@@ -76,78 +39,103 @@ async function getLiveRank(id: number, mode: number, country: string) {
     }
   }
 }
-export async function getBests({
-  id,
-  mode,
-  ruleset,
-  rankingSystem,
-  page,
-  perPage,
-}: {
-  id: Id
-  mode: Mode
-  ruleset: Ruleset
-  rankingSystem: RankingSystem
-  page: Range<0, 10>
-  perPage: Range<1, 11>
-}) {
-  const start = page * perPage
-  const _mode = toBanchoPyMode(mode, ruleset)
-  if (_mode === undefined)
-    throw new TRPCError({ code: 'PRECONDITION_FAILED', message: 'unsupported mode' })
 
-  const orderBy: Prisma.ScoreFindManyArgs['orderBy'] = {}
-  if (rankingSystem === 'ppv2')
-    orderBy.pp = 'desc'
-  else if (rankingSystem === 'rankedScore')
-    orderBy.score = 'desc'
-  else if (rankingSystem === 'totalScore')
-    orderBy.score = 'desc'
-  const scores = await db.score.findMany({
-    where: {
-      userId: id,
-      mode: _mode,
-      status: {
-        in: [2, 3],
-      },
-    },
-    include: {
-      beatmap: {
-        include: {
-          source: true,
+export default class BanchoPyUser implements UserDataProvider<Id> {
+  db: PrismaClient = prismaClient
+
+  async userExists({ handle, keys }: UserDataProvider.OptType<number, Record<never, never>>): Promise<boolean> {
+    return (await this.db.user.count(createUserQuery(handle, keys || ['id', 'name', 'safeName', 'email']))) > 0
+  }
+
+  async getBaseUser<Includes extends Partial<Record<keyof UserOptional<number>, boolean>>>(opt: UserDataProvider.OptType<Id, Includes>): Promise<BaseUser<number> | null> {
+    const { handle, includes, keys } = opt
+    const user = await this.db.user.findFirst(
+      createUserQuery(handle, keys || ['id', 'name', 'safeName', 'email']),
+    )
+    if (user == null)
+      return null
+
+    return toBaseUser({ user, includes })
+  }
+
+  async getBaseUsers<Includes extends Partial<Record<keyof UserOptional<Id>, boolean>>>(opt: { handle: string | number; includes?: Includes | undefined }): Promise<BaseUser<number>[]> {
+    const { handle, includes } = opt
+    const users = await this.db.user.findMany(createUserQuery(handle))
+    return users.map(user => toBaseUser({ user, includes }))
+  }
+
+  async getBests({
+    id,
+    mode,
+    ruleset,
+    rankingSystem,
+    page,
+    perPage,
+  }: {
+    id: Id
+    mode: Mode
+    ruleset: Ruleset
+    rankingSystem: RankingSystem
+    page: Range<0, 10>
+    perPage: Range<1, 11>
+  }) {
+    const start = page * perPage
+    const _mode = toBanchoPyMode(mode, ruleset)
+    if (_mode === undefined)
+      throw new TRPCError({ code: 'PRECONDITION_FAILED', message: 'unsupported mode' })
+
+    const orderBy: Prisma.ScoreFindManyArgs['orderBy'] = {}
+    if (rankingSystem === 'ppv2')
+      orderBy.pp = 'desc'
+    else if (rankingSystem === 'rankedScore')
+      orderBy.score = 'desc'
+    else if (rankingSystem === 'totalScore')
+      orderBy.score = 'desc'
+    const scores = await this.db.score.findMany({
+      where: {
+        userId: id,
+        mode: _mode,
+        status: {
+          in: [2, 3],
         },
       },
-    },
-    orderBy,
-    skip: start,
-    take: perPage,
-  })
-  return toRankingSystemScores({ scores, rankingSystem, mode })
-}
-
-export async function getStatisticsOfUser({
-  id,
-  country,
-}: {
-  id: Id
-  country: string
-}) {
-  const [results, ranks, livePPRank] = await Promise.all([
-    db.stat.findMany({
-      where: {
-        id,
+      include: {
+        beatmap: {
+          include: {
+            source: true,
+          },
+        },
       },
-    }),
+      orderBy,
+      skip: start,
+      take: perPage,
+    })
+    return toRankingSystemScores({ scores, rankingSystem, mode })
+  }
 
-    db.$queryRaw<
-    Array<{
-      id: Id
-      mode: number
-      ppv2Rank: bigint
-      totalScoreRank: bigint
-      rankedScoreRank: bigint
-    }>
-    >/* sql */`
+  async getStatisticsOfUser({
+    id,
+    country,
+  }: {
+    id: Id
+    country: string
+  }) {
+    const [results, ranks, livePPRank] = await Promise.all([
+      this.db.stat.findMany({
+        where: {
+          id,
+        },
+      }),
+
+      this.db.$queryRaw<
+        Array<{
+          id: Id
+          mode: number
+          ppv2Rank: bigint
+          totalScoreRank: bigint
+          rankedScoreRank: bigint
+        }>
+      >/* sql */`
   WITH ranks AS (
     SELECT
       id,
@@ -170,172 +158,170 @@ export async function getStatisticsOfUser({
   WHERE id = ${id}
   `,
 
-    redisClient
-      ? {
-          osu: {
-            standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
-            relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
-            autopilot: await getLiveRank(id, BanchoPyMode.osuAutopilot, country),
-          },
-          taiko: {
-            standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
-            relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
-          },
-          fruits: {
-            standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
-            relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
-          },
-          mania: {
-            standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
-          },
-        }
-      : undefined,
-  ])
+      redisClient
+        ? {
+            osu: {
+              standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
+              relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
+              autopilot: await getLiveRank(id, BanchoPyMode.osuAutopilot, country),
+            },
+            taiko: {
+              standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
+              relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
+            },
+            fruits: {
+              standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
+              relax: await getLiveRank(id, BanchoPyMode.osuRelax, country),
+            },
+            mania: {
+              standard: await getLiveRank(id, BanchoPyMode.osuStandard, country),
+            },
+          }
+        : undefined,
+    ])
 
-  const statistics: UserStatistic<Id, Mode, Ruleset, RankingSystem> = {
-    osu: {
-      standard: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.osuStandard,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.osuStandard),
-        livePPRank: livePPRank?.osu.standard,
-      }),
-      relax: createRulesetData({
-        databaseResult: results.find(i => i.mode === BanchoPyMode.osuRelax),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.osuRelax),
-        livePPRank: livePPRank?.osu.relax,
-      }),
-      autopilot: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.osuAutopilot,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.osuAutopilot),
-        livePPRank: livePPRank?.osu.autopilot,
-      }),
-    },
-    taiko: {
-      standard: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.taikoStandard,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.taikoStandard),
-        livePPRank: livePPRank?.taiko.standard,
-      }),
-      relax: createRulesetData({
-        databaseResult: results.find(i => i.mode === BanchoPyMode.taikoRelax),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.taikoRelax),
-        livePPRank: livePPRank?.taiko.relax,
-      }),
-    },
-    fruits: {
-      standard: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.fruitsStandard,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.fruitsStandard),
-        livePPRank: livePPRank?.fruits.standard,
-      }),
-      relax: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.fruitsRelax,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.fruitsRelax),
-        livePPRank: livePPRank?.fruits.relax,
-      }),
-    },
-    mania: {
-      standard: createRulesetData({
-        databaseResult: results.find(
-          i => i.mode === BanchoPyMode.maniaStandard,
-        ),
-        ranks: ranks.find(i => i.mode === BanchoPyMode.maniaStandard),
-        livePPRank: livePPRank?.mania.standard,
-      }),
-    },
+    const statistics: UserStatistic<Id, Mode, Ruleset, RankingSystem> = {
+      osu: {
+        standard: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.osuStandard,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.osuStandard),
+          livePPRank: livePPRank?.osu.standard,
+        }),
+        relax: createRulesetData({
+          databaseResult: results.find(i => i.mode === BanchoPyMode.osuRelax),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.osuRelax),
+          livePPRank: livePPRank?.osu.relax,
+        }),
+        autopilot: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.osuAutopilot,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.osuAutopilot),
+          livePPRank: livePPRank?.osu.autopilot,
+        }),
+      },
+      taiko: {
+        standard: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.taikoStandard,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.taikoStandard),
+          livePPRank: livePPRank?.taiko.standard,
+        }),
+        relax: createRulesetData({
+          databaseResult: results.find(i => i.mode === BanchoPyMode.taikoRelax),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.taikoRelax),
+          livePPRank: livePPRank?.taiko.relax,
+        }),
+      },
+      fruits: {
+        standard: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.fruitsStandard,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.fruitsStandard),
+          livePPRank: livePPRank?.fruits.standard,
+        }),
+        relax: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.fruitsRelax,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.fruitsRelax),
+          livePPRank: livePPRank?.fruits.relax,
+        }),
+      },
+      mania: {
+        standard: createRulesetData({
+          databaseResult: results.find(
+            i => i.mode === BanchoPyMode.maniaStandard,
+          ),
+          ranks: ranks.find(i => i.mode === BanchoPyMode.maniaStandard),
+          livePPRank: livePPRank?.mania.standard,
+        }),
+      },
+    }
+    return statistics
   }
-  return statistics
-}
 
-// high cost
-export async function getFullUser<
-  Excludes extends Partial<Record<keyof (UserExtra<Id> & UserOptional<Id>), boolean>>,
->({ handle, excludes }: { handle: string | Id; excludes?: Excludes }) {
-  type Additional = UserExtra<Id, Mode, Ruleset, RankingSystem> & Required<UserOptional<Id>>
-  if (!excludes)
-    excludes = <Excludes>{ secrets: true }
-  const user = await db.user.findFirst(createUserQuery(handle))
+  async getFullUser<Excludes extends Partial<Record<keyof (UserExtra<Id> & UserOptional<Id>), boolean>>>({ handle, excludes }: { handle: string | Id; excludes?: Excludes }) {
+    type Additional = UserExtra<Id, Mode, Ruleset, RankingSystem> & Required<UserOptional<Id>>
+    if (!excludes)
+      excludes = <Excludes>{ secrets: true }
+    const user = await this.db.user.findFirst(createUserQuery(handle))
 
-  if (user == null)
-    return null
+    if (user == null)
+      return null
 
-  const baseFullUser = await toFullUser(user)
-  const returnValue = {
-    statistics:
+    const baseFullUser = await toFullUser(user)
+    const returnValue = {
+      statistics:
         (excludes.statistics === true
           ? undefined
-          : await getStatisticsOfUser(user)) as Excludes['statistics'] extends true ? undefined : Additional['statistics'],
-    relationships:
+          : await this.getStatisticsOfUser(user)) as Excludes['statistics'] extends true ? undefined : Additional['statistics'],
+      relationships:
         (excludes.relationships === true
           ? undefined
           : await getRelationships(user)) as Excludes['relationships'] extends true ? never : Additional['relationships'],
-    email: (excludes.email === true ? undefined : user.email) as Excludes['email'] extends true ? never : Additional['email'],
-    profile: (excludes.profile === true
-      ? undefined
-      : {
-          html: user.userpageContent || '',
-          // TODO: alter database to read/save raw
-          raw: {},
-        }) as Excludes['profile'] extends true ? never : Additional['profile'],
-    secrets: (excludes.secrets === false
-      ? {
-          password: user.pwBcrypt,
-          apiKey: user.apiKey ?? undefined,
-        }
-      : undefined) as Excludes['secrets'] extends false ? Additional['secrets'] : never,
+      email: (excludes.email === true ? undefined : user.email) as Excludes['email'] extends true ? never : Additional['email'],
+      profile: (excludes.profile === true
+        ? undefined
+        : {
+            html: user.userpageContent || '',
+            // TODO: alter database to read/save raw
+            raw: {},
+          }) as Excludes['profile'] extends true ? never : Additional['profile'],
+      secrets: (excludes.secrets === false
+        ? {
+            password: user.pwBcrypt,
+            apiKey: user.apiKey ?? undefined,
+          }
+        : undefined) as Excludes['secrets'] extends false ? Additional['secrets'] : never,
+    }
+
+    return {
+      ...baseFullUser,
+      ...returnValue,
+    }
   }
 
-  return {
-    ...baseFullUser,
-    ...returnValue,
+  async updateUser(
+    user: BaseUser<Id>,
+    input: {
+      email?: string
+      name?: string
+      userpageContent?: string
+    },
+  ) {
+    const result = await this.db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        email: input.email,
+        name: input.name,
+        userpageContent: input.userpageContent,
+      },
+    })
+    return toBaseUser({ user: result })
   }
-}
 
-export async function updateUser(
-  user: BaseUser<Id>,
-  input: {
-    email?: string
-    name?: string
-    userpageContent?: string
-  },
-) {
-  const result = await db.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      email: input.email,
-      name: input.name,
-      userpageContent: input.userpageContent,
-    },
-  })
-  return toBaseUser({ user: result })
-}
-
-export async function updateUserPassword(
-  user: BaseUser<Id>,
-  newPasswordMD5: string,
-) {
+  async updateUserPassword(
+    user: BaseUser<Id>,
+    newPasswordMD5: string,
+  ) {
   // TODO: gen salt round
-  const salt = await bcrypt.genSalt()
-  const pwBcrypt = await bcrypt.hash(newPasswordMD5, salt)
-  const result = await db.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      pwBcrypt,
-    },
-  })
-  return toBaseUser({ user: result })
+    const salt = await bcrypt.genSalt()
+    const pwBcrypt = await bcrypt.hash(newPasswordMD5, salt)
+    const result = await this.db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        pwBcrypt,
+      },
+    })
+    return toBaseUser({ user: result })
+  }
 }
---
+++ src/adapters/base/client/user.ts
@@ -0,0 +1,77 @@
+import type { Mode, Range, RankingSystem, Ruleset } from '~/types/common'
+import type { BaseUser, UserExtra, UserOptional, UserStatistic } from '~~/src/types/user'
+import type { RankingSystemScore } from '~~/src/types/score'
+
+export namespace UserDataProvider {
+  export type ComposableProperties<Id> = UserExtra<Id> & UserOptional<Id>
+  export interface OptType<
+  IdType,
+  Includes extends Partial<Record<keyof UserOptional<IdType>, boolean>> = Record<
+    never,
+    never
+  >,
+> {
+    handle: string | IdType
+    includes?: Includes
+    keys?: Array<['id', 'name', 'safeName', 'email'][number]>
+  }
+}
+export interface UserDataProvider<Id = unknown> {
+  userExists({ handle, keys }: UserDataProvider.OptType<Id>): Promise<boolean>
+  getBaseUser<Includes extends Partial<Record<keyof UserOptional<number>, boolean>>>(opt: UserDataProvider.OptType<Id, Includes>): Promise<BaseUser<Id> | null>
+  getBaseUsers<Includes extends Partial<Record<keyof UserOptional<Id>, boolean>>>(opt: { handle: string | Id; includes?: Includes }): Promise<BaseUser<Id>[]>
+  getBests<_Mode extends Mode, _Ruleset extends Ruleset, _RankingSystem extends RankingSystem>({
+    id,
+    mode,
+    ruleset,
+    rankingSystem,
+    page,
+    perPage,
+  }: {
+    id: Id
+    mode: _Mode
+    ruleset: _Ruleset
+    rankingSystem: _RankingSystem
+    page: Range<0, 10>
+    perPage: Range<1, 11>
+  }): Promise<RankingSystemScore<
+    bigint,
+    Id,
+    _Mode,
+    _RankingSystem & 'ppv2'
+  >[]>
+
+  getStatisticsOfUser({
+    id,
+    country,
+  }: {
+    id: Id
+    country: string
+  }): Promise<UserStatistic<Id>>
+
+  getFullUser<Excludes extends Partial<Record<keyof UserDataProvider.ComposableProperties<Id>, boolean>>>({ handle, excludes }: { handle: string | Id; excludes?: Excludes }):
+  Promise<
+    (
+      BaseUser<Id>
+      & {
+        [K in keyof UserDataProvider.ComposableProperties<Id> as Exclude<Excludes[K], 'secrets'> extends true ? never : K]: UserDataProvider.ComposableProperties<Id>[K];
+      }
+      & Excludes['secrets'] extends true ? { secrets: UserDataProvider.ComposableProperties<Id>['secrets'] } : never
+    )
+    | null
+  >
+
+  updateUser(
+    user: BaseUser<Id>,
+    input: {
+      email?: string
+      name?: string
+      userpageContent?: string
+    },
+  ): Promise<BaseUser<Id>>
+
+  updateUser(
+    user: BaseUser<Id>,
+    newPasswordMD5: string,
+  ): Promise<BaseUser<Id>>
+}
