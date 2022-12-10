24b184456739a97349e430a0b20611deef089f9c
arily <ariy@arily.moe>
2022-12-11 04:23:03 +0900

    fmt type

--- src/adapters/base/client/user-relations.ts
+++ src/adapters/base/client/user-relations.ts
@@ -2,8 +2,8 @@ import type { BaseUser, UserRelationship } from '~/types/user'
 import type { Awaitable, Relationship } from '~/types/common'
 
 export abstract class UserRelationshipDataProvider<Id> {
-  abstract getRelationships({ user }: { user: { id: Id } }): Awaitable<UserRelationship<Id>[]>
+  abstract getRelationships(query: { user: { id: Id } }): Awaitable<UserRelationship<Id>[]>
   abstract getOneRelationship(fromUser: { id: Id }, toUser: { id: Id }): Awaitable<Relationship | void>
-  abstract removeOneRelationship({ fromUser, targetUser, type }: { fromUser: BaseUser<Id>; targetUser: BaseUser<Id>; type: Relationship }): Awaitable<void>
-  abstract countRelationship({ user, type }: { user: BaseUser<Id>; type: Relationship }): Awaitable<number>
+  abstract removeOneRelationship(query: { fromUser: BaseUser<Id>; targetUser: BaseUser<Id>; type: Relationship }): Awaitable<void>
+  abstract countRelationship(query: { user: BaseUser<Id>; type: Relationship }): Awaitable<number>
 }
