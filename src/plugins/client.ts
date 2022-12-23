6fba09ac35ae1450cd485fcfc672c8e98b4b1ebe
arily <ariy@arily.moe>
2022-12-23 14:59:41 +0900

    update trpc fix ssr

--- src/plugins/client.ts
+++ src/plugins/client.ts
@@ -1,10 +1,10 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
+import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
 import superjson from 'superjson'
 import type { AppRouter } from '@/server/trpc/routers'
 
 export default defineNuxtPlugin(() => {
-  const session = useCookie('session')
-  const client = createTRPCProxyClient<AppRouter>({
+  // const session = useCookie('session')
+  const client = createTRPCNuxtClient<AppRouter>({
     transformer: superjson,
     links: [
       httpBatchLink({
@@ -12,17 +12,18 @@ export default defineNuxtPlugin(() => {
          * If you want to use SSR, you need to use the server's full URL
          * @link https://trpc.io/docs/ssr
          **/
-        url: 'http://localhost:3000/api/trpc',
-        fetch(url, options) {
-          return fetch(url, {
-            ...options,
-            headers: {
-              ...options?.headers || {},
-              cookie: (options?.headers as Record<string, string>)?.cookie || `session=${session.value}`,
-            },
-            credentials: 'include',
-          })
-        },
+        url: '/api/trpc',
+        maxURLLength: 2083, // a suitable size
+        // fetch(url, options) {
+        //   return fetch(url, {
+        //     ...options,
+        //     headers: {
+        //       ...options?.headers || {},
+        //       cookie: (options?.headers as Record<string, string>)?.cookie || `session=${session.value}`,
+        //     },
+        //     credentials: 'include',
+        //   })
+        // },
       }),
     ],
   })
