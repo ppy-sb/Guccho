bd6e1cdbf72f1fbfc90efd123894cec496eb8bf0
arily <ariy@arily.moe>
2023-01-25 17:02:38 +0900

    score

--- src/adapters/ppy.sb@bancho.py/client/index.ts
+++ src/adapters/ppy.sb@bancho.py/client/index.ts
@@ -1,12 +1,13 @@
 import {
   PrismaClient,
 } from '@prisma/client' // ppy.sb
-import * as BanchoPy from '~/adapters/bancho.py@mysql5.7/client'
+import * as Parent from '~/adapters/bancho.py@mysql5.7/client'
 
 export const prismaClient = new PrismaClient()
 
-export const LeaderboardDataProvider = BanchoPy.LeaderboardDataProvider
-export const UserRelationshipDataProvider = BanchoPy.UserRelationshipDataProvider
-// export const UserDataProvider = BanchoPy.UserDataProvider
+export const LeaderboardDataProvider = Parent.LeaderboardDataProvider
+export const UserRelationshipDataProvider = Parent.UserRelationshipDataProvider
+export const ScoreDataProvider = Parent.ScoreDataProvider
+export const MapDataProvider = Parent.MapDataProvider
+
 export { UserDataProvider } from './user'
-export const MapDataProvider = BanchoPy.MapDataProvider
