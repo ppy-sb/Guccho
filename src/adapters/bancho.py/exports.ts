157064c5b5cb8ccadd2085a933fc7dfbf696df42
arily <ariy@arily.moe>
2023-01-27 11:28:09 +0900

    fixup plural

--- src/adapters/bancho.py/exports.ts
+++ src/adapters/bancho.py/exports.ts
@@ -1,13 +1,10 @@
 import type { Feature } from '~/types/common'
-import { modes, rulesets } from '~/types/common'
+export { modes, rulesets } from '~/types/common'
 
 export type Id = number
 export type ScoreId = bigint
 
 export const features: Set<Feature> = new Set(['userpage'])
-
-export const modes = modes
-export const rulesets = rulesets
 
 export * from '~/adapters/bancho.py/transforms/id-conversion'
 export * from './checks'
