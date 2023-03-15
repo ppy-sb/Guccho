32ad7ac50d7f88d75663141c92330df614df51b1
arily <aril@arily.moe>
2023-03-15 14:51:13 +0900

    optimize safari detection

--- src/composables/useSafariDetector.ts
+++ src/composables/useSafariDetector.ts
@@ -1,9 +1,8 @@
 const detectClient = () => navigator.userAgent.search('Safari') >= 0 && navigator.userAgent.search('Chrome') < 0
-const detectServer = () => {
-  const headers = useRequestHeaders(['user-agent'])
-  return headers['user-agent']?.includes('Safari')
-}
+const detectServer = () => useRequestHeaders(['user-agent'])['user-agent']?.includes('Safari')
 
-export default function useSafari(def = false) {
-  return detectServer() || detectClient() || def
-}
+export default (def = false) =>
+  (process.server
+    ? detectServer()
+    : detectClient())
+  || def
