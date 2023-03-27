cf12ab15d7b0f1c2c2ba5543537211eaed179cb3
arily <aril@arily.moe>
2023-03-27 12:42:40 +0900

    not using prismaErrorHandler

--- src/adapters/bancho.py/server/prisma.ts
+++ src/adapters/bancho.py/server/prisma.ts
@@ -1,32 +1,32 @@
-import { TRPCError } from '@trpc/server'
+// import { TRPCError } from '@trpc/server'
 import type { PrismaClient as ImportedPrismaClient } from '.prisma/bancho.py'
-import type { PrismaClientExtensionError, PrismaClientInitializationError, PrismaClientKnownRequestError } from '.prisma/bancho.py/runtime'
+// import type { PrismaClientExtensionError, PrismaClientInitializationError, PrismaClientKnownRequestError } from '.prisma/bancho.py/runtime'
 import { createCursedRequire } from '~/server'
 
 const require = createCursedRequire(import.meta.url ?? __filename)
-const { PrismaClient, Prisma } = require<{
+const { PrismaClient /* Prisma */ } = require<{
   PrismaClient: typeof ImportedPrismaClient
-  Prisma: {
-    PrismaClientExtensionError: typeof PrismaClientExtensionError
-    PrismaClientInitializationError: typeof PrismaClientInitializationError
-    PrismaClientKnownRequestError: typeof PrismaClientKnownRequestError
-  }
+  // Prisma: {
+  //   PrismaClientExtensionError: typeof PrismaClientExtensionError
+  //   PrismaClientInitializationError: typeof PrismaClientInitializationError
+  //   PrismaClientKnownRequestError: typeof PrismaClientKnownRequestError
+  // }
 }>('.prisma/bancho.py')
 
-export const prismaErrorHandler = (cb: (e: unknown) => Error) => (e: unknown) => {
-  if (
-    e instanceof Prisma.PrismaClientInitializationError
-    || e instanceof Prisma.PrismaClientKnownRequestError
-    || e instanceof Prisma.PrismaClientExtensionError
-  ) {
-    return new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: e.message,
-      cause: e.name,
-    })
-  }
-  return cb(e)
-}
+// export const prismaErrorHandler = (cb: (e: unknown) => Error) => (e: unknown) => {
+//   if (
+//     e instanceof Prisma.PrismaClientInitializationError
+//     || e instanceof Prisma.PrismaClientKnownRequestError
+//     || e instanceof Prisma.PrismaClientExtensionError
+//   ) {
+//     return new TRPCError({
+//       code: 'INTERNAL_SERVER_ERROR',
+//       message: e.message,
+//       cause: e.name,
+//     })
+//   }
+//   return cb(e)
+// }
 
 let prismaClientInstance: ImportedPrismaClient
 export const getPrismaClient = () => {
