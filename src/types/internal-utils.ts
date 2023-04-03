5c969dee2d759ecf13aace27d398888c9f3c082c
arily <aril@arily.moe>
2023-04-04 00:35:16 +0900

    fmt

--- src/types/internal-utils.ts
+++ src/types/internal-utils.ts
@@ -5,9 +5,7 @@ export type Enumerate<
   ? Acc[number]
   : Enumerate<N, [...Acc, Acc['length']]>
 
-export type U2I<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I
-) => void
+export type U2I<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void
   ? I
   : never
 
