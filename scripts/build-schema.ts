188f107bd3bd2146ad5ce2ea6c7ed29cd5a5765c
arily <aril@arily.moe>
2023-04-07 13:07:38 +0900

    scripting

--- scripts/build-schema.ts
+++ scripts/build-schema.ts
@@ -1,13 +1,8 @@
-import { exec } from 'child_process'
-import { readdirSync } from 'fs'
-import { join, relative } from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import { exec } from 'node:child_process'
+import { readdirSync } from 'node:fs'
+import { join, relative } from 'node:path'
 
 const root = join(__dirname, '..')
-
 const schemaPath = join(__dirname, '../prisma')
 
 const files = readdirSync(schemaPath)
@@ -16,7 +11,3 @@ for (const file of files) {
   const schema = relative(root, join('prisma/', file))
   exec(`npx prisma generate --schema=${schema}`)
 }
-// series([
-//   () => exec('npm run dev'),
-//   () => exec('npm run test'),
-// ])
