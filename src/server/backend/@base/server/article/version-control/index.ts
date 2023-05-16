43901ce0a5d518fccec29add5210cda2a554fcd6
arily <aril@arily.moe>
2023-05-16 17:09:46 +0900

    optimize

--- src/server/backend/@base/server/article/version-control/index.ts
+++ src/server/backend/@base/server/article/version-control/index.ts
@@ -1,25 +1,19 @@
 import { createUpdatePath } from './path'
 import { v0, v1 } from './v'
 
+const filterSelf = <T>(i: T): i is Exclude<T, 'self'> => i !== 'self'
 export const paths = [
-  createUpdatePath(v0, v1, (from) => {
-    console.log('update from v0 to v1', from)
-    type TFrom = typeof from
-    type TPriv = TFrom['privilege']['read'] | TFrom['privilege']['write']
-    const filterSelf = <T extends TPriv[number]>(i: T): i is Exclude<T, 'self'> => i !== 'self'
-
-    return {
-      html: from.html,
-      json: from.json,
-      owner: from.owner,
-      privilege: {
-        read: from.privilege.read.filter(filterSelf),
-        write: from.privilege.write.filter(filterSelf),
-      },
-      dynamic: false as const,
-      v: 1,
-      created: [from.owner, new Date()] as [string | number, Date],
-      lastUpdated: [from.owner, new Date()] as [string | number, Date],
-    }
-  }),
+  createUpdatePath(v0, v1, from => ({
+    html: from.html,
+    json: from.json,
+    owner: from.owner,
+    privilege: {
+      read: from.privilege.read.filter(filterSelf),
+      write: from.privilege.write.filter(filterSelf),
+    },
+    dynamic: false as const,
+    v: 1,
+    created: [from.owner, new Date()] as [string | number, Date],
+    lastUpdated: [from.owner, new Date()] as [string | number, Date],
+  })),
 ]
