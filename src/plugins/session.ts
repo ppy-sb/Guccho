3804424a9553fdf12003856144cf246375b52eb0
arily <aril@arily.moe>
2023-07-12 16:46:17 +0900

    no use

--- src/plugins/session.ts
+++
@@ -1,9 +0,0 @@
-import { useSession } from '~/store/session'
-
-export default defineNuxtPlugin(async () => {
-  const session = useSession()
-  if (session.loggedIn) {
-    return
-  }
-  await session.retrieve()
-})
