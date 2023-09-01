04d50030898c0e624f27496c0c7625f1e82645e5
william <133595458+miwate@users.noreply.github.com>
2023-09-01 17:44:28 +0200

    Update fr-FR.ts

--- src/server/backend/$base/locales/fr-FR.ts
+++ src/server/backend/$base/locales/fr-FR.ts
@@ -1,5 +1,5 @@
 export default {
   landing: {
-    content: 'Nous créons un serveur privé osu! avec des fonctionnalités jamais vues auparavant!',
+    content: 'Nous créons un serveur privé osu! avec des fonctionnalités pour tous les modes. Vous trouverez des algorithmes Relax et Autopilot avancés pour le calcul des pp. Vous pouvez également changer de pseudo comme bon vous semble.',
   },
 } as const
