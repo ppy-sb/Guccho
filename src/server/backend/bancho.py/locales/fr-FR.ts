40c28330f964a661d959dd881c5ada6b493f5ef9
william <133595458+miwate@users.noreply.github.com>
2023-09-01 17:48:04 +0200

    Update fr-FR.ts

--- src/server/backend/bancho.py/locales/fr-FR.ts
+++ src/server/backend/bancho.py/locales/fr-FR.ts
@@ -1,12 +1,11 @@
 import type { DeepPartial } from '@trpc/server'
-
 import type { GlobalI18n } from '~/locales/@types'
 
 export default {
   landing: {
-    content: `Nous créons un serveur privé osu! avec des fonctionnalités jamais vues auparavant!
-- pour plus d'informations, regardez bancho.py et Guccho sur GitHub
-- nous sommes entièrement Open source`,
+    content: `Nous créons un serveur privé osu! avec des fonctionnalités pour tous les modes. Vous trouverez des algorithmes Relax et Autopilot avancés pour le calcul des pp. Vous pouvez également changer de pseudo comme bon vous semble.
+- pour plus d'informations，veuillez visiter gulag et la page GitHub de Guccho。
+- notre projet est open source!`,
   },
   service: {
   },
