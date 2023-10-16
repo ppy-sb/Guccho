f83e6e9a700a3a3ed7402ede6e475270002e7fe1
arily <arily@arily.moe>
2023-10-16 12:15:58 +0800

    add prisma log output

--- src/server/backend/bancho.py/env.ts
+++ src/server/backend/bancho.py/env.ts
@@ -1,4 +1,5 @@
-import { discriminatedUnion, literal, object, string } from 'zod'
+import type { Prisma } from 'prisma-client-bancho-py'
+import { array, discriminatedUnion, literal, object, string } from 'zod'
 import { zodFQDN as FQDN, zodPath as path } from '~/server/trpc/shapes'
 import { validator as base, redis, redisURL } from '$base/env'
 import env from '~~/env'
@@ -26,10 +27,16 @@ export const apiEndpoint = object({
   v1: string().url().optional(),
 }).optional()
 
+export const log = object({
+  prisma: array(literal('info').or(literal('query')).or(literal('warn')).or(literal('error'))).refine((arg): arg is Prisma.LogLevel[] => true),
+}).partial().optional()
+
 export const validator = base.and(object({
   dsn,
   avatar,
   api: apiEndpoint,
+  log,
 }).and(rank))
+
 
 export const config = lazySingleton(() => validator.parse(env))
--- src/server/backend/bancho.py/server/source/prisma.ts
+++ src/server/backend/bancho.py/server/source/prisma.ts
@@ -1,3 +1,14 @@
+import { env as processEnv } from 'node:process'
+import type { Prisma } from 'prisma-client-bancho-py'
 import { PrismaClient } from 'prisma-client-bancho-py'
+import env from '~~/env'
 
-export const getPrismaClient = lazySingleton(() => new PrismaClient())
+const log: Prisma.LogLevel[] = []
+
+if ('LOG_PRISMA_QUERY' in processEnv || env.log?.prisma?.includes('query')) {
+  log.push('query')
+}
+
+export const getPrismaClient = lazySingleton(() => new PrismaClient({
+  log,
+}))
