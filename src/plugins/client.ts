0bb334c4a798110a1fa5f3e7f59490b7d08a20fe
arily <arily@arily.moe>
2023-10-27 17:47:17 +0900

    use batch link

--- src/plugins/client.ts
+++ src/plugins/client.ts
@@ -1,4 +1,4 @@
-import { createTRPCNuxtClient, httpBatchLink, httpLink } from 'trpc-nuxt/client'
+import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
 import { transformer } from '~/server/trpc/transformer'
 import type { AppRouter } from '~/server/trpc/routers'
 
@@ -6,9 +6,6 @@ export default defineNuxtPlugin(() => {
   const client = createTRPCNuxtClient<AppRouter>({
     transformer,
     links: [
-      httpLink({
-        url: '/api/trpc',
-      }),
       httpBatchLink({
         url: '/api/trpc',
         maxURLLength: 2083, // a suitable size
