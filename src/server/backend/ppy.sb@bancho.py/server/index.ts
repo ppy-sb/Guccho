d5a7ff4657526c556611c5c194b7e3de96f7906f
arily <arily@arily.moe>
2023-12-21 17:16:07 +0900

    remove re-export

--- src/server/backend/ppy.sb@bancho.py/server/index.ts
+++ src/server/backend/ppy.sb@bancho.py/server/index.ts
@@ -1,15 +1,27 @@
-export {
-  AdminProvider,
-  ArticleProvider,
-  FileProvider,
-  LogProvider,
-  MapProvider,
-  MonitorProvider,
-  RankProvider,
-  ScoreProvider,
-  SessionProvider,
-  UserRelationProvider,
-  ClanProvider,
+import {
+  AdminProvider as BAdminProvider,
+  ArticleProvider as BArticleProvider,
+  ClanProvider as BClanProvider,
+  FileProvider as BFileProvider,
+  LogProvider as BLogProvider,
+  MapProvider as BMapProvider,
+  MonitorProvider as BMonitorProvider,
+  RankProvider as BRankProvider,
+  ScoreProvider as BScoreProvider,
+  SessionProvider as BSessionProvider,
+  UserRelationProvider as BUserRelationProvider,
 } from '~/server/backend/bancho.py/server'
 
 export { UserProvider } from './user'
+
+export class AdminProvider extends BAdminProvider {}
+export class ArticleProvider extends BArticleProvider {}
+export class ClanProvider extends BClanProvider {}
+export class FileProvider extends BFileProvider {}
+export class LogProvider extends BLogProvider {}
+export class MapProvider extends BMapProvider {}
+export class MonitorProvider extends BMonitorProvider {}
+export class RankProvider extends BRankProvider {}
+export class ScoreProvider extends BScoreProvider {}
+export class SessionProvider extends BSessionProvider {}
+export class UserRelationProvider extends BUserRelationProvider {}
