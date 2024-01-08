a1190346c5e3472b6629570f25409bac42a01016
arily <arily@arily.moe>
2024-01-09 00:34:05 +0900

    add nvim project config

---
+++ .lazy.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        vue = { "eslint_d" },
+      },
+    },
+  },
+}
