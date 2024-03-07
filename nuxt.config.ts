843b74aea48cc63097dbcb0632a125a1add77151
arily <arily@arily.moe>
2024-03-07 20:08:08 +0800

    prevent ios autozoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,6 +64,8 @@ export default defineNuxtConfig({
       mode: 'out-in', // default
     },
     head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
       meta: [
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
       ],
