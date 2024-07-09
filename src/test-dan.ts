631aa5566b19556ff3c9ae0ca2cf5c13ac9be0c5
arily <arily@arily.moe>
2024-07-09 08:52:14 +0800

    separate test script from impl

--- src/test-dan.ts
+++ src/test-dan.ts
@@ -1,22 +1,13 @@
 /* eslint-disable no-console */
-import { Grade, StableMod } from './def/score'
-import { BeatmapSource, type ReferencedBeatmapCompact } from './def/beatmap'
-import type { UserCompact } from './def/user'
-import { Mode } from './def'
-import {
-  Achievement,
-  type AchievementResult,
-  type ComputedCond,
-  type Cond,
-  type DetailResult,
-  OP,
-  type Usecase,
-  type ValidatingScore,
-} from './def/dan'
+import { pretty_result, run_usecase } from '~/common/utils/dan'
+import { Mode } from '~/def'
+import { BeatmapSource, type ReferencedBeatmapCompact } from '~/def/beatmap'
+import { Achievement, OP } from '~/def/dan'
+import { Grade, StableMod } from '~/def/score'
+import type { UserCompact } from '~/def/user'
 
 const danBaseValidator = [
   OP.Commented,
-
   [
     OP.AND, [
       [OP.ModeEq, Mode.Mania],
@@ -32,9 +23,7 @@ const danBaseValidator = [
       ],
     ],
   ],
-
   'dan common',
-
 ] as const
 
 const rf9 = {
@@ -173,216 +162,6 @@ const _9DanScore = {
 }
 const result10dan = run_usecase(rf10, _10DanScore)
 const result9dan = run_usecase(rf9, _10DanScore)
-
-function pretty_result(
-  res: AchievementResult[],
-  usecase: Usecase,
-  score: ValidatingScore
-) {
-  const msg: string[] = []
-
-  msg.push(`usecase: ${usecase.name}`)
-  msg.push(`description: ${usecase.name}`)
-  msg.push(
-    `score: Score(player=${score.player.name}(${score.player.id}), score=${score.score}, accuracy=${score.accuracy}, nonstop=${score.nonstop})`
-  )
-  msg.push(
-    `beatmap: Beatmap(creator=${score.beatmap.creator}, version=${score.beatmap.version}, mode=${score.beatmap.mode}, md5=${score.beatmap.md5})`
-  )
-
-  return msg.concat(...res.map(res => fmtDetail(res, 1).flat()))
-}
-
-function sp(n: number) {
-  return '  '.repeat(n)
-}
-function b(_b: boolean) {
-  return _b ? '✓' : '✗'
-}
-
-function fmtDetail(
-  detail: AchievementResult | DetailResult<ComputedCond>,
-  indent: number = 0
-) {
-  let msg: string[] = []
-
-  if ('achievement' in detail) {
-    msg.push(`${sp(indent)}Achievement(${Achievement[detail.achievement]}):`)
-    indent += 1
-  }
-  if ('cond' in detail) {
-    msg.push(
-      `${sp(indent)}${b(detail.result)} ${fmt_cond(
-        detail.cond,
-        (detail as any).value,
-        (detail as any).remark
-      )}`
-    )
-    indent += 1
-  }
-
-  if ('detail' in detail) {
-    if (Array.isArray(detail.detail)) {
-      msg = msg.concat(...detail.detail.map(d => fmtDetail(d, indent + 1)))
-    }
-    else if (typeof detail.detail === 'object') {
-      msg = msg.concat(fmtDetail(detail.detail as any, indent + 1))
-    }
-  }
-
-  return msg
-}
-
-function fmt_cond(cond: Cond, value: Cond[1], remark?: string) {
-  const [op, _maybeValue] = cond
-  switch (op) {
-    case OP.AccGte:
-    case OP.BeatmapMd5Eq:
-    case OP.ScoreGte:
-      return `${OP[op]} ${_maybeValue}, ${value}`
-
-    case OP.Commented:
-      return `Plug(${remark})`
-
-    case OP.WithMod:
-      return `${OP[op]} ${StableMod[_maybeValue]}`
-
-    case OP.ModeEq:
-      return `${OP[op]} ${_maybeValue}, ${value}`
-
-    // op without attribute
-    case OP.NoPause:
-      return `${OP[op]}`
-
-    // referenced op
-    case OP.Extends:
-      return `${OP[op]} ${Achievement[_maybeValue]}`
-
-    // deep op
-    case OP.AND:
-    case OP.OR:
-    case OP.NOT:
-      return `${OP[op]}`
-  }
-}
-
-function run_usecase<AB extends readonly [Achievement, Cond]>(
-  usecase: Usecase<AB>,
-  score: ValidatingScore
-): AchievementResult<AB>[] {
-  const check_result: AchievementResult<AB>[] = []
-  for (const [achievement, check_cond] of usecase.achievements) {
-    const r = run_cond<AB[1], AB>(check_cond, usecase.achievements, score)
-    check_result.push({
-      achievement,
-      result: r.result,
-      detail: r,
-    } as any)
-  }
-  return check_result
-}
-
-function run_cond<C extends Cond, AB extends readonly [Achievement, Cond]>(
-  cond: C,
-  achievements: readonly AB[],
-  score: ValidatingScore
-): DetailResult<C, AB> {
-  const [op, _maybeValue] = cond
-  switch (op) {
-    case OP.BeatmapMd5Eq: {
-      return {
-        cond,
-        result: score.beatmap.md5 === _maybeValue,
-        value: score.beatmap.md5,
-      } as DetailResult<C, AB>
-    }
-    case OP.AccGte: {
-      return {
-        cond,
-        result: score.accuracy >= _maybeValue,
-        value: score.accuracy,
-      } as DetailResult<C, AB>
-    }
-    case OP.ScoreGte: {
-      return {
-        cond,
-        result: score.score >= _maybeValue,
-        value: score.score,
-      } as DetailResult<C, AB>
-    }
-    case OP.NoPause: {
-      return {
-        cond,
-        result: score.nonstop,
-        value: score.nonstop,
-      } as DetailResult<C, AB>
-    }
-    case OP.WithMod: {
-      return {
-        cond,
-        result: score.mods.includes(_maybeValue),
-        value: _maybeValue,
-      } as DetailResult<C, AB>
-    }
-    case OP.ModeEq: {
-      return {
-        cond,
-        result: score.mode === _maybeValue,
-        value: _maybeValue,
-      } as DetailResult<C, AB>
-    }
-    case OP.Commented: {
-      const result = run_cond(_maybeValue, achievements, score)
-      return {
-        cond,
-        result: result.result,
-        detail: result,
-        remark: cond[2],
-      } as unknown as DetailResult<C, AB>
-    }
-    case OP.NOT: {
-      const result = run_cond(_maybeValue, achievements, score)
-      return {
-        cond,
-        result: !result.result,
-        detail: result,
-      } as DetailResult<C, AB>
-    }
-    case OP.AND: {
-      const results = _maybeValue.map(c => run_cond(c, achievements, score))
-      return {
-        cond,
-        result: results.every(r => r.result),
-        detail: results,
-      } as unknown as DetailResult<C, AB>
-    }
-    case OP.OR: {
-      const results = _maybeValue.map(c => run_cond(c, achievements, score))
-      return {
-        cond,
-        result: results.some(r => r.result),
-        detail: results,
-      } as unknown as DetailResult<C, AB>
-    }
-    case OP.Extends: {
-      const _cond
-        = achievements.find(
-          ([achievement]) => achievement === _maybeValue
-        )?.[1]
-        ?? raiseError(
-          `extending achievement (${Achievement[_maybeValue]}) not found`
-        )
-      const result = run_cond(_cond, achievements, score)
-      return {
-        cond: [OP.Extends, _maybeValue],
-        result: result.result,
-        detail: result,
-      } as unknown as DetailResult<C, AB>
-    }
-    default:
-      assertNotReachable(op)
-  }
-}
 
 console.log(pretty_result(result10dan, rf10, _10DanScore).flat().join('\n'))
 console.log('-------------------------')
