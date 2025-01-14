4a9bbbfb51150aca142c60797d611cf3fad2aca1
arily <arily@arily.moe>
2025-01-14 19:59:57 +0800

    add export function

---
+++ src/pages/admin/dan.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth', 'staff'],
+})
+</script>
+
+<template>
+  <div class="container mx-auto custom-container">
+    <a class="btn" href="/api/admin/dan/export">
+      Export all
+    </a>
+  </div>
+</template>
