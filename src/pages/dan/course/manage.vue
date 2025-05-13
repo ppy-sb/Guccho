2c2314fd3a555e2916dbf97004fde820592b63aa
arily <arily@arily.moe>
2025-05-13 15:02:27 +0800

    fixup dan course mange layout

--- src/pages/dan/course/manage.vue
+++ src/pages/dan/course/manage.vue
@@ -158,8 +158,8 @@ zh-CN:
         </div>
         <input id="keyword" v-model="query.keyword" name="keyword" type="search" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="t('search-text')">
         <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          {{ t('search') }}
-          <Icon icon="ion:search-outline" class="w-4 h-4" />
+          <span class="inline">{{ t('search') }}</span>
+          <Icon icon="ion:search-outline" class="w-4 h-4 inline" />
         </button>
       </div>
     </form>
