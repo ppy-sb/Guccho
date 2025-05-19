4fcbc3381f3f54e3b98ec29c3447dcd7c52803e2
arily <arily@arily.moe>
2025-05-20 02:47:25 +0800

    move recalc to processor

--- src/server/backend/ppy.sb@bancho.py/server/dan/processor/$base.ts
+++ src/server/backend/ppy.sb@bancho.py/server/dan/processor/$base.ts
@@ -1,9 +1,18 @@
 import { type DanProvider } from '..'
+import { type DanProvider as Base } from '$base/server/dan'
+import { type Cond, type DatabaseDan, type DatabaseRequirementCondBinding, type Requirement } from '~/def/dan'
 import { Logger } from '~/server/backend/ppy.sb@bancho.py/log'
 
-export abstract class BaseDanProcessor {
+export namespace BaseDanProcessor {
+  export interface RecalcProvidedDanParam<Id, ScoreId> extends Omit<Base.RecalcQualifiedScoresParam<Id, ScoreId>, 'dan'> {
+    dan: DatabaseDan<Id, DatabaseRequirementCondBinding<Id, Requirement, Cond>>
+  }
+}
+export abstract class BaseDanProcessor<Id, ScoreId> {
   logger = Logger.child({ label: 'dan:processor' })
   constructor(readonly dp: DanProvider) {}
+  abstract recalcDan(opt: Base.RecalcQualifiedScoresParam<Id, ScoreId>): Promise<void>
+  abstract recalcProvidedDan(opt: BaseDanProcessor.RecalcProvidedDanParam<Id, ScoreId>): Promise<void>
   abstract init(): Promise<void>
   abstract dispose(): Promise<void>
 }
