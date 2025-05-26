f29334d8b402d5a2b28f5d2c567d57dfae2b184d
arily <arily@arily.moe>
2025-05-27 00:23:24 +0800

    filter out unfinished scores

--- src/server/backend/ppy.sb@bancho.py/server/dan/processor/realtime.ts
+++ src/server/backend/ppy.sb@bancho.py/server/dan/processor/realtime.ts
@@ -27,7 +27,9 @@ export class RealtimeDanProcessor extends CacheSyncedDanProcessor implements Cac
   }
 
   async onScoreSubmitted(row: InsertEvent<InferSelectModel<typeof schema.scores>>) {
-    const scores = row.affectedRows.map(item => item.after)
+    const scores = row.affectedRows
+      .map(item => item.after)
+      .filter(item => item.grade !== 'F')
 
     const [beatmaps, users, patcherScoresMeta] = await Promise.all([
       this.dp.drizzle.query.beatmaps.findMany({
