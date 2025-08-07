81dad9d4b64fff9e37b2ea4e810172105f956365
arily <arily@arily.moe>
2025-08-08 02:13:47 +0800

    debug: log scores

--- src/server/backend/ppy.sb@bancho.py/server/dan/processor/realtime.ts
+++ src/server/backend/ppy.sb@bancho.py/server/dan/processor/realtime.ts
@@ -31,6 +31,10 @@ export class RealtimeDanProcessor extends CacheSyncedDanProcessor implements Cac
       .map(item => item.after)
       .filter(item => item.grade !== 'F')
 
+    this.logger.debug({
+      message: `received full scores: ${scores.map(item => item.id).join(', ')}`,
+    })
+
     const [beatmaps, users, patcherScoresMeta] = await Promise.all([
       this.dp.drizzle.query.beatmaps.findMany({
         where: inArray(schema.beatmaps.md5, scores.map(item => item.mapMd5)),
