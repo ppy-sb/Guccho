61a94a90707879c782737b16bbb4d6275faab5cb
arily <arily@arily.moe>
2025-09-21 21:51:50 +0800

    ppy.sb doesn't support bpy api key ATM

--- src/server/backend/ppy.sb@bancho.py/dynamic-settings.ts
+++ src/server/backend/ppy.sb@bancho.py/dynamic-settings.ts
@@ -1,1 +1,10 @@
-export * from '../bancho.py/dynamic-settings'
+// export * from '../bancho.py/dynamic-settings'
+
+import { settings as bpy } from '../bancho.py/dynamic-settings'
+
+const excludeApiKey = Object.fromEntries(
+  Object.entries(bpy).filter(([key]) => key !== 'apiKey')
+)
+export const settings = {
+  ...excludeApiKey,
+}
