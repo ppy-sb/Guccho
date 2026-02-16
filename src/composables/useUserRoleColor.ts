ec85129318fb8b713158767d1a2dfd76636989e5
arily <arily@arily.moe>
2026-02-17 01:38:16 +0800

    give owner color to supporter

--- src/composables/useUserRoleColor.ts
+++ src/composables/useUserRoleColor.ts
@@ -5,13 +5,6 @@ export default function (user: UserCompact<unknown>) {
   const roles = user.roles
   const classNames = ['text-transparent bg-clip-text animate-role-text']
 
-  if (roles.includes(UserRole.Owner)) {
-    return [
-      ...classNames,
-      'bg-gradient-to-b from-yellow-300 via-pink-500 to-red-400 dark:from-yellow-200 dark:via-pink-300 dark:to-red-400',
-    ]
-  }
-
   if (roles.includes(UserRole.Admin)) {
     return [
       ...classNames,
@@ -23,6 +16,13 @@ export default function (user: UserCompact<unknown>) {
     return [
       ...classNames,
       'bg-gradient-to-b from-green-400 to-purple-500 dark:from-green-200 dark:to-purple-400',
+    ]
+  }
+
+  if (roles.includes(UserRole.Supporter)) {
+    return [
+      ...classNames,
+      'bg-gradient-to-b from-yellow-300 via-pink-500 to-red-400 dark:from-yellow-200 dark:via-pink-300 dark:to-red-400',
     ]
   }
 
